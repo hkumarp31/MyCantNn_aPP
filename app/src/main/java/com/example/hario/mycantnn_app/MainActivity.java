7afd4b8133fd5cc8bea84adeec70a91727f361c3
Hemant Kumar <hkumarp.31@gmail.com>
2018-02-14 11:16:34 +0530

    Initial change

--- app/src/main/java/com/example/hario/mycantnn_app/MainActivity.java
+++ app/src/main/java/com/example/hario/mycantnn_app/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         // startActivity(host);
         startActivity(new Intent(MainActivity.this, Host_login.class));
         finish();
+        //initial change
 
     }
 
